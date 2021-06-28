624e1d8e6082b55bb3fe4309fc50b2aa660f0036
assis <assis.siebra@visagio.com>
2021-06-28 10:44:37 -0400

    check

--- config/initializers/config_mail.rb
+++ config/initializers/config_mail.rb
@@ -1,10 +1,10 @@
 ActionMailer::Base.smtp_settings = {
-    address: "#{ENV.address}",
-    port: "#{ENV.port}",
-    authentication: "#{ENV.authentication}",
-    enable_starttls_auto: true,
-    user_name: "#{ENV.user_name}",
-    password: "#{ENV.password}",
-    domain: "#{ENV.domain}",
+    # address: "#{ENV.address}",
+    # port: "#{ENV.port}",
+    # authentication: "#{ENV.authentication}",
+    # enable_starttls_auto: true,
+    # user_name: "#{ENV.user_name}",
+    # password: "#{ENV.password}",
+    # domain: "#{ENV.domain}",
 }
 
