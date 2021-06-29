f83a57dc4882d22cba4d4161b1c983fdc941a60a
assis <assis.siebra@visagio.com>
2021-06-28 21:41:17 -0400

    colocando chave

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    ENV["GOOGLE_CLIENT_SECRET"]='WnO8SCJE6rW222thc9mlkDw2'
-    ENV["GOOGLE_CLIENT_ID"]='879658048775-2gobq6e3imis2a0brii0a751aq5gumsm.apps.googleusercontent.com'
+    ENV["GOOGLE_CLIENT_SECRET"]='WYp0tO-1Ck43DY5T59iJ4Kvt'
+    ENV["GOOGLE_CLIENT_ID"]='242278242806-h26c9aargobttri4sclsrr9shnp2d3f3.apps.googleusercontent.com'
     provider :google_oauth2, ENV["GOOGLE_CLIENT_ID"], ENV["GOOGLE_CLIENT_SECRET"]
 end
