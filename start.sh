375d7dcf7882ecb0f80132412a506cd3eb4bccb3
assis <assis.siebra@visagio.com>
2021-07-04 02:45:39 -0400

    fix: puting the assets:precompile dockerfile

--- start.sh
+++ start.sh
@@ -9,7 +9,16 @@ docker build  -f Dockerfile.ngninx -t nginx_meritmoney:v1 .
 
 
 docker run --name meritmoney -d --link postgres meritmoney:v1
-docker run --name nginx_meritmoney -d -p 4000:80  --link meritmoney nginx_meritmoney:v1
+docker run --name nginx_meritmoney -d -p 80:80  --link meritmoney nginx_meritmoney:v1
 
+
+
+
+#docker exec -it meritmoney rails db:migrate
+#docker exec -it meritmoney rails db:create
+
+# docker exec -i -t meritmoney /bin/bash
+# docker exec -i -t 6af6b0b5eb1c /bin/bash
+# docker container prune
 
 
