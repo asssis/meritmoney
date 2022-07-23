2a98199176bea561fe445440bc0c834db40e885d
Assis Junior Siebra Barbosa <assisjuniorb@gmail.com>
2022-07-22 20:42:51 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
-docker stop meritmoney nginx_meritmoney postgres
-docker rm meritmoney nginx_meritmoney postgres
+#docker stop meritmoney nginx_meritmoney postgres
+#docker rm meritmoney nginx_meritmoney postgres
 docker rmi meritmoney:v1 nginx_meritmoney:v1
 
 
-docker run --name postgres -e POSTGRES_PASSWORD=postgres -d -v="pgdata:/var/lib/postgresql/data" postgres
-docker build  -f Dockerfile.rails -t meritmoney:v1 .
-docker build  -f Dockerfile.ngninx -t nginx_meritmoney:v1 .
+#docker run --name postgres -e POSTGRES_PASSWORD=postgres -d -v="pgdata:/var/lib/postgresql/data" postgres
+docker build  -f Dockerfile.rails -p 3000:3000 -t meritmoney:v1 .
+#docker build  -f Dockerfile.ngninx -t nginx_meritmoney:v1 .
 
 
-docker run --name meritmoney -d --link postgres meritmoney:v1
-docker run --name nginx_meritmoney -d -p 80:80  --link meritmoney nginx_meritmoney:v1
+#docker run --name meritmoney -d --link postgres meritmoney:v1
+#docker run --name nginx_meritmoney -d -p 80:80  --link meritmoney nginx_meritmoney:v1
 
 
 
